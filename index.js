2d5d53e1846b722ff2b05baf6798a447c7440900
jacobm719 <119356895+jacobm719@users.noreply.github.com>
2022-12-14 20:38:24 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -108,26 +108,28 @@ const View = (() => {
     const formEl = document.querySelector(".form");
     const todoListEl = document.querySelector(".todo-list");
     const finishedListEl = document.querySelector(".finished-list");
+    const editButton = `<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>`;
+    const deleteButton = `<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>`;
     const updateTodoList = (todos) => {
         let template = "";
         let templateFinished = "";
         todos.forEach((todo) => {
             if (!todo.done) {
                 if (todo.status) {
-                    const todoTemplate = `<li><span><input type="text" id="listid${todo.id}" value="${todo.title}"></span><button class="btn--edit" id="${todo.id}">edit</button><button class="btn--delete" id="${todo.id}">remove</button></li>`;
+                    const todoTemplate = `<li><span><input type="text" id="listid${todo.id}" value="${todo.title}"></span><button class="btn--edit" id="${todo.id}">${editButton}</button><button class="btn--delete" id="${todo.id}">${deleteButton}</button></li>`;
                     template += todoTemplate;
                 }
                 else {
-                    const todoTemplate = `<li><span><input type="text" id="listid${todo.id}" value="${todo.title}" readonly></span><button class="btn--edit" id="${todo.id}">edit</button><button class="btn--delete" id="${todo.id}">remove</button></li>`;
+                    const todoTemplate = `<li><span><input type="text" id="listid${todo.id}" value="${todo.title}" readonly></span><button class="btn--edit" id="${todo.id}">${editButton}</button><button class="btn--delete" id="${todo.id}">${deleteButton}</button></li>`;
                     template += todoTemplate;
                 }
             } else {
-                const finishedTemplate = `<li><span><input type="text" id="listid${todo.id}" value="${todo.title}" readonly></span><button class="btn--delete" id="${todo.id}">remove</button></li>`;
+                const finishedTemplate = `<li><span><input type="text" id="listid${todo.id}" value="${todo.title}" readonly></span><button class="btn--delete" id="${todo.id}">${deleteButton}</button></li>`;
                 templateFinished += finishedTemplate;
             }
         });
         if (template == "") {
-            template = "no task to display";
+            template = "no active tasks";
         }
         todoListEl.innerHTML = template;
         finishedListEl.innerHTML = templateFinished;
@@ -242,8 +244,10 @@ const ViewModel = ((View, Model) => {
                             break;
                         }
                     }
-                    let done = !state.todos[curIndex].done;
-                    Model.finished(id, done);
+                    if (state.todos[curIndex].done || !state.todos[curIndex].status) {
+                        let done = !state.todos[curIndex].done;
+                        Model.finished(id, done);
+                    }
                 }
             });
         });
